02e20eedafac307d88668204e798e821f3f835f2
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-24 16:17:35 -0300

    agregue ciudades de la primer, tercer y ultima posicion del array en el documento

--- js/02-ejercicio.js
+++ js/02-ejercicio.js
@@ -7,4 +7,8 @@ do{
 }
     while(confirm(`Quieres ingresar otra ciudad?`))
 
-        document.writeln(`La cantidad de ciudades que ingresaste es: ${ciudades.length}`)
+        document.writeln(`La cantidad de ciudades que ingresaste es: ${ciudades.length}<Br>`)
+
+        document.writeln(`<br>Elemento 1° posición: ${ciudades[0]}`)
+        document.writeln(`<br>Elemento 3° posición: ${ciudades[2]}`)
+        document.writeln(`<br>Elemento ultima posición: ${ciudades[ciudades.length - 1]}`)
