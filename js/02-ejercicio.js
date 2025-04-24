15e347cd6f913ffac9505fc7ae10ccf3ac64b11b
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-24 16:31:24 -0300

    agregue la lista de ciudades ingresadas y un alert en caso de que el valor sea invalido

--- js/02-ejercicio.js
+++ js/02-ejercicio.js
@@ -4,6 +4,11 @@ let ciudades = []
 do{
     i = prompt("ingrese ciudad:");
     ciudades.push(i);
+    if (i === null || !isNaN(i) ) {
+        alert("Fin del programa. Ingresaste un valor invalido.");
+        ciudades.pop(ciudades[ciudades.length -1])
+        break;
+      }
 }
     while(confirm(`Quieres ingresar otra ciudad?`))
 
@@ -11,4 +16,17 @@ do{
 
         document.writeln(`<br>Elemento 1° posición: ${ciudades[0]}`)
         document.writeln(`<br>Elemento 3° posición: ${ciudades[2]}`)
+        document.writeln(`<br>Elemento ultima posición: ${ciudades[ciudades.length - 1]}<br>`)
+
+        ciudades.push ('Paris')
         document.writeln(`<br>Elemento ultima posición: ${ciudades[ciudades.length - 1]}`)
+
+        document.writeln(`<h2>Lista de Ciudades</h2>`)
+document.writeln(`<ul>`)
+
+for( i=0;i<ciudades.length;i++){
+    document.writeln(`<li>${ciudades[i]}</li>`)
+}
+document.writeln(`</ul>`)
+
+document.writeln(`<ul>`)
