719fd851952519e55cc661a48963e59d6c3173e2
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-25 14:09:03 -0300

    cree programa que con una funcion devulve si el numero es par o impar

---
+++ js/04-ejercicio.js
@@ -0,0 +1,15 @@
+const  num = parseInt(prompt(`Ingresa un numero`))
+
+if(num===null || isNaN(num)){
+    alert(`Fin del programa. Ingresaste un numero invalido.`)
+}
+
+function resto (numero1,numero2){
+    const resultado = numero1 % numero2
+    return resultado
+}
+
+const resultado = resto(num,2)
+
+if(resultado===0){document.writeln(`El numero "${num}" es par.`)}
+else{document.writeln(`El numero "${num}" es impar.`)}
