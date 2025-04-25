af5c138efd3cd08754898878df455cc58e845ca3
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-25 14:14:10 -0300

    agregue un condicional que termina el programa si ingresan un numero invalido

--- js/04-ejercicio.js
+++ js/04-ejercicio.js
@@ -2,14 +2,17 @@ const  num = parseInt(prompt(`Ingresa un numero`))
 
 if(num===null || isNaN(num)){
     alert(`Fin del programa. Ingresaste un numero invalido.`)
+    document.writeln(`Por favor recarga la pagina🔃`)
 }
+else{
 
-function resto (numero1,numero2){
-    const resultado = numero1 % numero2
-    return resultado
+    function resto (numero1,numero2){
+        const resultado = numero1 % numero2
+        return resultado
+    }
+
+    const resultado = resto(num,2)
+
+    if(resultado===0){document.writeln(`El numero "${num}" es par.`)}
+    else{document.writeln(`El numero "${num}" es impar.`)}
 }
-
-const resultado = resto(num,2)
-
-if(resultado===0){document.writeln(`El numero "${num}" es par.`)}
-else{document.writeln(`El numero "${num}" es impar.`)}
